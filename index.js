5c18ea93b8cf5a1df40442ac50c4f5f8c2da5f39
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-29 12:04:43 -0300

    :tada: Commit inical

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path =  require("path");
+const app = express();
+
+// defining engine template
+app.set("view engine", "ejs");
+
+// defining static files
+// app.use(express.static(path.join(__dirname, "views")));
+// Obs: used  if you don't have the engine template
+
+// defining public files
+app.use(express.static(path.join(__dirname, "public")));
+
+// MVC - Model View Controler
+
+app.get("/", (req, res) => {
+   res.render("views/index");
+})
+
+app.get("/about", (req, res) => {
+   res.send("About")
+})
+
+// 404 error (page nout found)
+app.use((res, res) => {
+   res.send("404 Page not found");
+})
+
+// running the server
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log("Server is listening an port 8080"));
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+console.log("Right, it worked");
